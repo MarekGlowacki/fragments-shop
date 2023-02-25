ee8b790c2b273e8541e9516629fd60ea8d476365
User <user@its.me>
2023-02-25 09:48:20 +0100

    Initial commit

---
+++ src/main/java/online/javafun/fragments/FragmentsShopApplication.java
@@ -0,0 +1,10 @@
+package online.javafun.fragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FragmentsShopApplication.class, args
